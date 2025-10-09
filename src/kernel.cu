b17d4349a278ad0ec59c7d421327c52e8edacddf
Ryang Sohn <loop.infinitely@gmail.com>
2025-10-09 23:24:42 +0900

    style(kernel): ordering

--- src/kernel.cu
+++ src/kernel.cu
@@ -3,6 +3,22 @@
 
 #include <stdexcept>
 
+__global__ void gemm(__nv_bfloat16 *__restrict__ out,
+                     const __nv_bfloat16 *__restrict__ in_a,
+                     const __nv_bfloat16 *__restrict__ in_b,
+                     const __nv_bfloat16 *__restrict__ bias,
+                     __nv_bfloat16 scale, std::size_t m, std::size_t n,
+                     std::size_t k) {
+  const auto x = blockIdx.x * blockDim.x + threadIdx.x;
+  const auto y = blockIdx.y * blockDim.y + threadIdx.y;
+  if (x < m && y < n) {
+    auto res = bias ? bias[x * n + y] : __nv_bfloat16{0};
+    for (int i = 0; i < k; i++)
+      res += scale * in_a[x * k + i] * in_b[i * n + y];
+    out[x * n + y] = res;
+  }
+}
+
 void launch_gemm(Tensor &out, const Tensor &in_a, const Tensor &in_b,
                  const Tensor &bias, __nv_bfloat16 scale) {
   if (in_a.dimensions != 2 || in_b.dimensions != 2 || out.dimensions != 2)
@@ -20,20 +36,4 @@ void launch_gemm(Tensor &out, const Tensor &in_a, const Tensor &in_b,
   gemm<<<num_blocks, threads_per_block>>>(out.storage->data, in_a.storage->data,
                                           in_b.storage->data,
                                           bias.storage->data, scale, m, n, k);
-}
-
-__global__ void gemm(__nv_bfloat16 *__restrict__ out,
-                     const __nv_bfloat16 *__restrict__ in_a,
-                     const __nv_bfloat16 *__restrict__ in_b,
-                     const __nv_bfloat16 *__restrict__ bias,
-                     __nv_bfloat16 scale, std::size_t m, std::size_t n,
-                     std::size_t k) {
-  const auto x = blockIdx.x * blockDim.x + threadIdx.x;
-  const auto y = blockIdx.y * blockDim.y + threadIdx.y;
-  if (x < m && y < n) {
-    auto res = bias ? bias[x * n + y] : __nv_bfloat16{0};
-    for (int i = 0; i < k; i++)
-      res += scale * in_a[x * k + i] * in_b[i * n + y];
-    out[x * n + y] = res;
-  }
 }
