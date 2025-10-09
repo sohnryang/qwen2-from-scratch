9a42677a36e353bfd9ea36d526381cc6cb2d0197
Ryang Sohn <loop.infinitely@gmail.com>
2025-10-09 18:23:47 +0900

    feat(kernel): add GEMM kernel

--- include/kernel.h
+++ include/kernel.h
@@ -1,2 +1,10 @@
 #pragma once
 
+#include <cstddef>
+#include <cuda_bf16.h>
+
+__global__ void gemm(__nv_bfloat16 *__restrict__ out,
+                     const __nv_bfloat16 *__restrict__ in_a,
+                     const __nv_bfloat16 *__restrict__ in_b,
+                     const __nv_bfloat16 *__restrict__ bias, std::size_t m,
+                     std::size_t n, std::size_t k);
--- src/kernel.cu
+++ src/kernel.cu
@@ -1,1 +1,16 @@
 #include "kernel.h"
+
+__global__ void gemm(__nv_bfloat16 *__restrict__ out,
+                     const __nv_bfloat16 *__restrict__ in_a,
+                     const __nv_bfloat16 *__restrict__ in_b,
+                     const __nv_bfloat16 *__restrict__ bias, std::size_t m,
+                     std::size_t n, std::size_t k) {
+  const auto x = blockIdx.x * blockDim.x + threadIdx.x;
+  const auto y = blockIdx.y * blockDim.y + threadIdx.y;
+  if (x < m && y < n) {
+    auto res = bias ? bias[x * n + y] : __nv_bfloat16{0};
+    for (int i = 0; i < k; i++)
+      res += in_a[x * k + i] * in_b[i * n + y];
+    out[x * n + y] = res;
+  }
+}
