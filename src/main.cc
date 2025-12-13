84781a7e28edec261749083e625d9defe69ab368
Ryang Sohn <loop.infinitely@gmail.com>
2025-12-13 17:07:35 +0900

    feat(main): add basic model client
    There must be memory problem LOL

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,16 @@
-#include <cuda_runtime.h>
+#include "model.h"
+#include "tensor.h"
 
+#include <cstddef>
 #include <cstdlib>
+#include <fstream>
+#include <ios>
 #include <iostream>
+#include <memory>
+#include <stdexcept>
 #include <string>
+
+#include <tokenizers_cpp.h>
 
 #if __has_include(<unistd.h>)
 #include <unistd.h>
@@ -16,18 +24,38 @@ static void print_usage() {
 }
 #endif
 
+static std::unique_ptr<tokenizers::Tokenizer>
+load_tokenizer(const std::string &filename) {
+  std::ifstream tokenizer_file(filename, std::ios::binary | std::ios::ate);
+  const auto tokenizer_file_size = tokenizer_file.tellg();
+  tokenizer_file.seekg(0);
+  std::string tokenizer_blob;
+  tokenizer_blob.resize(tokenizer_file_size);
+  if (!tokenizer_file.read(reinterpret_cast<char *>(tokenizer_blob.data()),
+                           tokenizer_file_size))
+    throw std::runtime_error("failed to read tokenizer file");
+  return tokenizers::Tokenizer::FromBlobJSON(tokenizer_blob);
+}
+
 int main(int argc, char **argv) {
-  std::string weights_filename, input_filename;
+  std::string weights_filename, tokenizer_filename, input_filename,
+      system_prompt = "You're a helpful assistant.";
 #if __has_include(<unistd.h>)
   int next_option;
   do {
-    next_option = getopt(argc, argv, "w:i:h");
+    next_option = getopt(argc, argv, "w:t:i:p:h");
     switch (next_option) {
     case 'w':
       weights_filename = optarg;
       continue;
+    case 't':
+      tokenizer_filename = optarg;
+      continue;
     case 'i':
       input_filename = optarg;
+      continue;
+    case 'p':
+      system_prompt = optarg;
       continue;
     case '?':
     case 'h':
@@ -41,8 +69,38 @@ int main(int argc, char **argv) {
     std::cout << "Path to weights: ";
     std::cin >> weights_filename;
   }
-  if (input_filename.empty()) {
-    std::cout << "Path to input: ";
-    std::cin >> input_filename;
+  if (tokenizer_filename.empty()) {
+    std::cout << "Path to tokenizer: ";
+    std::cin >> tokenizer_filename;
+  }
+
+  constexpr std::size_t MAX_TOKENS = 128;
+  const auto model_weights = load_from_safetensors(weights_filename);
+  auto model = Qwen2Model::from_parameters(model_weights, MAX_TOKENS);
+
+  const auto tokenizer = load_tokenizer(tokenizer_filename);
+
+  const std::string BOS = "<|im_start|>", EOS = "<|im_end|>";
+  std::string prompt = BOS + "system\n" + system_prompt + EOS + "\n";
+  const auto tokenized_prompt = tokenizer->Encode(prompt);
+  if (!model.prefill(tokenized_prompt)) {
+    std::cerr << "System prompt too long." << std::endl;
+    std::exit(1);
+  }
+
+  while (true) {
+    std::cout << "User: ";
+    std::string user_prompt;
+    std::getline(std::cin, user_prompt);
+
+    const auto tokenized_user_prompt = tokenizer->Encode(
+        BOS + "user\n" + user_prompt + EOS + "\n" + BOS + "assistant\n");
+    auto generated_tokens = model.generate(tokenized_user_prompt);
+    if (generated_tokens.empty()) {
+      std::cout << "LLM: prompt too long, please try again." << std::endl;
+      continue;
+    }
+    generated_tokens.pop_back();
+    std::cout << "LLM: " << tokenizer->Decode(generated_tokens) << std::endl;
   }
 }
