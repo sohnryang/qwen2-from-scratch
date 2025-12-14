20d097fa3fdf47bc57a1bc68a9f25a33ec6fd3d6
Ryang Sohn <loop.infinitely@gmail.com>
2025-12-14 22:49:30 +0900

    style(kernel): use early-return style

--- src/kernel.cu
+++ src/kernel.cu
@@ -14,13 +14,14 @@ __global__ void gemm(__nv_bfloat16 *__restrict__ out,
                      std::size_t k) {
   const auto row = blockIdx.x * blockDim.x + threadIdx.x;
   const auto col = blockIdx.y * blockDim.y + threadIdx.y;
-  if (row < m && col < n) {
-    float res = bias ? __bfloat162float(bias[col]) : 0.0f;
-    for (int i = 0; i < k; i++)
-      res += __bfloat162float(scale) * __bfloat162float(in_a[row * k + i]) *
-             __bfloat162float(in_b[i * n + col]);
-    out[row * n + col] = __float2bfloat16(res);
-  }
+  if (row >= m || col >= n)
+    return;
+
+  float res = bias ? __bfloat162float(bias[col]) : 0.0f;
+  for (int i = 0; i < k; i++)
+    res += __bfloat162float(scale) * __bfloat162float(in_a[row * k + i]) *
+           __bfloat162float(in_b[i * n + col]);
+  out[row * n + col] = __float2bfloat16(res);
 }
 
 __global__ void gemm_transposed(__nv_bfloat16 *__restrict__ out,
@@ -31,13 +32,14 @@ __global__ void gemm_transposed(__nv_bfloat16 *__restrict__ out,
                                 std::size_t n, std::size_t k) {
   const auto row = blockIdx.x * blockDim.x + threadIdx.x;
   const auto col = blockIdx.y * blockDim.y + threadIdx.y;
-  if (row < m && col < n) {
-    float res = bias ? __bfloat162float(bias[col]) : 0.0f;
-    for (int i = 0; i < k; i++)
-      res += __bfloat162float(scale) * __bfloat162float(in_a[row * k + i]) *
-             __bfloat162float(in_b[col * k + i]);
-    out[row * n + col] = __float2bfloat16(res);
-  }
+  if (row >= m || col >= n)
+    return;
+
+  float res = bias ? __bfloat162float(bias[col]) : 0.0f;
+  for (int i = 0; i < k; i++)
+    res += __bfloat162float(scale) * __bfloat162float(in_a[row * k + i]) *
+           __bfloat162float(in_b[col * k + i]);
+  out[row * n + col] = __float2bfloat16(res);
 }
 
 __global__ void dense(__nv_bfloat16 *__restrict__ out,
@@ -47,14 +49,14 @@ __global__ void dense(__nv_bfloat16 *__restrict__ out,
                       std::size_t in_features, std::size_t out_features) {
   const auto row = blockIdx.x * blockDim.x + threadIdx.x;
   const auto col = blockIdx.y * blockDim.y + threadIdx.y;
-  if (row < n && col < out_features) {
-    auto res = bias ? __bfloat162float(bias[col]) : 0.0f;
-    for (int i = 0; i < in_features; i++)
-      res += __bfloat162float(x[row * in_features + i]) *
-             __bfloat162float(weight[col * in_features + i]);
-    out[row * out_features + col] =
-        __float2bfloat16(res / (1.0f + __expf(-res)));
-  }
+  if (row >= n || col >= out_features)
+    return;
+
+  auto res = bias ? __bfloat162float(bias[col]) : 0.0f;
+  for (int i = 0; i < in_features; i++)
+    res += __bfloat162float(x[row * in_features + i]) *
+           __bfloat162float(weight[col * in_features + i]);
+  out[row * out_features + col] = __float2bfloat16(res / (1.0f + __expf(-res)));
 }
 
 void launch_gemm(Tensor<__nv_bfloat16> &out, const Tensor<__nv_bfloat16> &in_a,
@@ -161,56 +163,62 @@ __global__ void elementwise_product(__nv_bfloat16 *__restrict__ out,
                                     const __nv_bfloat16 *__restrict__ y,
                                     float scale, std::size_t n) {
   const auto idx = blockIdx.x * blockDim.x + threadIdx.x;
-  if (idx < n)
-    out[idx] = __float2bfloat16(scale * __bfloat162float(x[idx]) *
-                                __bfloat162float(y[idx]));
+  if (idx >= n)
+    return;
+
+  out[idx] = __float2bfloat16(scale * __bfloat162float(x[idx]) *
+                              __bfloat162float(y[idx]));
 }
 
 __global__ void elementwise_add(__nv_bfloat16 *out, const __nv_bfloat16 *x,
                                 const __nv_bfloat16 *y, std::size_t n) {
   const auto idx = blockIdx.x * blockDim.x + threadIdx.x;
-  if (idx < n)
-    out[idx] =
-        __float2bfloat16(__bfloat162float(x[idx]) + __bfloat162float(y[idx]));
+  if (idx >= n)
+    return;
+
+  out[idx] =
+      __float2bfloat16(__bfloat162float(x[idx]) + __bfloat162float(y[idx]));
 }
 
 __global__ void softmax(float *out, const float *x, std::size_t batches,
                         std::size_t n) {
   const auto batch = blockIdx.x * blockDim.x + threadIdx.x;
-  if (batch < batches) {
-    float x_max = -INFINITY;
-    float norm = 0.0f;
-    for (int i = 0; i < n; i++) {
-      const auto x_cur = x[batch * n + i];
-      if (x_cur > x_max) {
-        norm = norm * __expf(x_max - x_cur);
-        x_max = x_cur;
-      }
-      norm += __expf(x_cur - x_max);
+  if (batch >= batches)
+    return;
+
+  float x_max = -INFINITY;
+  float norm = 0.0f;
+  for (int i = 0; i < n; i++) {
+    const auto x_cur = x[batch * n + i];
+    if (x_cur > x_max) {
+      norm = norm * __expf(x_max - x_cur);
+      x_max = x_cur;
     }
-    for (int i = 0; i < n; i++)
-      out[batch * n + i] = __expf(x[batch * n + i] - x_max) / norm;
+    norm += __expf(x_cur - x_max);
   }
+  for (int i = 0; i < n; i++)
+    out[batch * n + i] = __expf(x[batch * n + i] - x_max) / norm;
 }
 
 __global__ void softmax(__nv_bfloat16 *out, const __nv_bfloat16 *x,
                         std::size_t batches, std::size_t n) {
   const auto batch = blockIdx.x * blockDim.x + threadIdx.x;
-  if (batch < batches) {
-    __nv_bfloat16 x_max = -INFINITY;
-    float norm = 0.0f;
-    for (int i = 0; i < n; i++) {
-      const auto x_cur = x[batch * n + i];
-      if (x_cur > x_max) {
-        norm = norm * __expf(__bfloat162float(x_max - x_cur));
-        x_max = x_cur;
-      }
-      norm += __expf(x_cur - x_max);
+  if (batch >= batches)
+    return;
+
+  __nv_bfloat16 x_max = -INFINITY;
+  float norm = 0.0f;
+  for (int i = 0; i < n; i++) {
+    const auto x_cur = x[batch * n + i];
+    if (x_cur > x_max) {
+      norm = norm * __expf(__bfloat162float(x_max - x_cur));
+      x_max = x_cur;
     }
-    for (int i = 0; i < n; i++)
-      out[batch * n + i] = __float2bfloat16(
-          __expf(__bfloat162float(x[batch * n + i] - x_max)) / norm);
+    norm += __expf(x_cur - x_max);
   }
+  for (int i = 0; i < n; i++)
+    out[batch * n + i] = __float2bfloat16(
+        __expf(__bfloat162float(x[batch * n + i] - x_max)) / norm);
 }
 
 void launch_softmax(Tensor<__nv_bfloat16> &out,
@@ -240,16 +248,17 @@ __global__ void grouped_query_attention_scores(
   const auto k_head = q_head / groups;
   const auto row = idx / sequence_length_kv % sequence_length_q;
   const auto col = idx % sequence_length_kv;
-  if (q_head < q_heads) {
-    float score = 0.0f;
-    for (int i = 0; i < dimension; i++)
-      score += __bfloat162float(
-                   q[row * q_heads * dimension + q_head * dimension + i]) *
-               __bfloat162float(
-                   k[col * kv_heads * dimension + k_head * dimension + i]);
-    out[row * q_heads * sequence_length_kv + q_head * sequence_length_kv +
-        col] = score / sqrtf(dimension);
-  }
+  if (q_head >= q_heads)
+    return;
+
+  float score = 0.0f;
+  for (int i = 0; i < dimension; i++)
+    score += __bfloat162float(
+                 q[row * q_heads * dimension + q_head * dimension + i]) *
+             __bfloat162float(
+                 k[col * kv_heads * dimension + k_head * dimension + i]);
+  out[row * q_heads * sequence_length_kv + q_head * sequence_length_kv + col] =
+      score / sqrtf(dimension);
 }
 
 __global__ void grouped_query_attention_scores_masked(
@@ -263,21 +272,22 @@ __global__ void grouped_query_attention_scores_masked(
   const auto k_head = q_head / groups;
   const auto row = idx / sequence_length_kv % sequence_length_q;
   const auto col = idx % sequence_length_kv;
-  if (q_head < q_heads) {
-    float score = 0.0f;
-    for (int i = 0; i < dimension; i++)
-      score += __bfloat162float(
-                   q[row * q_heads * dimension + q_head * dimension + i]) *
-               __bfloat162float(
-                   k[col * kv_heads * dimension + k_head * dimension + i]);
-    out[row * q_heads * sequence_length_kv + q_head * sequence_length_kv +
-        col] = score / sqrtf(dimension) +
-               (static_cast<int>(col) - static_cast<int>(row) >
-                        static_cast<int>(sequence_length_kv) -
-                            static_cast<int>(sequence_length_q)
-                    ? -INFINITY
-                    : 0);
-  }
+  if (q_head >= q_heads)
+    return;
+
+  float score = 0.0f;
+  for (int i = 0; i < dimension; i++)
+    score += __bfloat162float(
+                 q[row * q_heads * dimension + q_head * dimension + i]) *
+             __bfloat162float(
+                 k[col * kv_heads * dimension + k_head * dimension + i]);
+  out[row * q_heads * sequence_length_kv + q_head * sequence_length_kv + col] =
+      score / sqrtf(dimension) +
+      (static_cast<int>(col) - static_cast<int>(row) >
+               static_cast<int>(sequence_length_kv) -
+                   static_cast<int>(sequence_length_q)
+           ? -INFINITY
+           : 0);
 }
 
 __global__ void grouped_query_attention_output(
@@ -291,16 +301,17 @@ __global__ void grouped_query_attention_output(
   const auto v_head = q_head / groups;
   const auto row = idx / dimension % sequence_length_q;
   const auto col = idx % dimension;
-  if (q_head < q_heads) {
-    float o = 0.0f;
-    for (int i = 0; i < sequence_length_kv; i++)
-      o += p[row * q_heads * sequence_length_kv + q_head * sequence_length_kv +
-             i] *
-           __bfloat162float(
-               v[i * kv_heads * dimension + v_head * dimension + col]);
-    out[row * q_heads * dimension + q_head * dimension + col] =
-        __float2bfloat16(o);
-  }
+  if (q_head >= q_heads)
+    return;
+
+  float o = 0.0f;
+  for (int i = 0; i < sequence_length_kv; i++)
+    o += p[row * q_heads * sequence_length_kv + q_head * sequence_length_kv +
+           i] *
+         __bfloat162float(
+             v[i * kv_heads * dimension + v_head * dimension + col]);
+  out[row * q_heads * dimension + q_head * dimension + col] =
+      __float2bfloat16(o);
 }
 
 __global__ void precompute_rope_bases(float *__restrict__ cos_basis_out,
